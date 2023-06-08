756c7ba85a1035d31b3975da475bdbb40012c2ba
wobsoriano <sorianorobertc@gmail.com>
2023-06-08 09:13:56 -0700

    update test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,33 @@
-import { describe, test } from 'vitest'
+/**
+ * @vitest-environment jsdom
+ */
+
+import { describe, expect, test, vi } from 'vitest'
+import { renderComposable } from 'vue-test-composables'
+import { useScreenOrientation } from '../src'
 
 describe('useScreenOrientation', () => {
-  test.todo('portrait')
-  test.todo('landscape')
+  test('portrait', () => {
+    window.matchMedia = vi.fn().mockImplementation(query => ({
+      matches: query === '(orientation: portrait)',
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+    }))
+
+    const { result } = renderComposable(() => useScreenOrientation())
+
+    expect(result.value).toBe('portrait')
+  })
+
+  test('landscape', () => {
+    window.matchMedia = vi.fn().mockImplementation(query => ({
+      matches: query === '(orientation: landscape)',
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+    }))
+
+    const { result } = renderComposable(() => useScreenOrientation())
+
+    expect(result.value).toBe('landscape')
+  })
 })
